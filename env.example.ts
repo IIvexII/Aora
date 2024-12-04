fcec5adfa8b7e674cf75efd01c3842601781cb75
Zafeer <contact@zafeer.pk>
2024-12-04 09:02:11 +0500

    chore: add env example

---
+++ env.example.ts
@@ -0,0 +1,7 @@
+export const APPWRITE_ENDPOINT = "https://cloud.appwrite.io/v1";
+export const APPWRITE_PACKAGE_NAME = "com.example.app";
+export const APPWRITE_PROJECT_ID = "your_project_id";
+export const APPWRITE_DATABASE_ID = "your_database_id";
+export const APPWRITE_USER_COLLECTION_ID = "your_user_collection_id";
+export const APPWRITE_VIDEO_COLLECTION_ID = "your_video_collection_id";
+export const APPWRITE_STORAGE_ID = "your_storage_id";
